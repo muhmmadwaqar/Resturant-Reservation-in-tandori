2688bcf00f832801fad95ce5cde9c938123368f7
Muhammad Waqar <itswaqarmalik11@gmail.com>
2024-07-08 18:58:59 +0500

    Initial commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">waqar</div>
+          <div className="right">
+            <p>Amar Sidhu,Lahore ,05459 |</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By CODE WITH waqar</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By Code With waqar.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
